b13a74b80b6035a0c4a77fb79fa08b04de163d7d
Marc Sevilleja <marc@sevilayha.com>
2020-05-15 23:57:16 -0700

    Added waypoints and pauses
    Characters follow specific paths and can set a time between starting movement again. Also added a multiplier for the animation speed for slowing down/speeding up

---
+++ Assets/Scripts/CharacterPathing.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPathing : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] List<float> waitTimes;
+
+    Animator anim;
+    int waypointIndex = 0;
+    int waitTimeIndex = 0;
+    bool readyToMove = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set position of character to start of path
+        transform.position = waypoints[waypointIndex].transform.position;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (readyToMove == true && waypointIndex < waypoints.Count)
+        {
+            // Get value of current position
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            // Speed move speed independent of frame
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                StartCoroutine(WaitForNextMove());
+                waypointIndex++;
+            }
+        }
+    }
+
+    IEnumerator WaitForNextMove()
+    {
+        readyToMove = false;
+        anim.SetBool("Moving", false);
+
+        yield return new WaitForSeconds(waitTimes[waitTimeIndex]);
+        if (waitTimeIndex < waitTimes.Count - 1)
+            waitTimeIndex++;
+        readyToMove = true;
+        anim.SetBool("Moving", true);
+    }
+}
