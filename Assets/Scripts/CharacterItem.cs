5144252697bf15f11816d9eb6e5590707bf87d22
Marc Sevilleja <marc@sevilayha.com>
2020-05-17 23:42:47 -0700

    Implemented Steal Script

---
+++ Assets/Scripts/CharacterItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterItem : MonoBehaviour
+{
+    GameObject childItem = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponentInChildren<GameObject>() != null)
+            childItem = GetComponentInChildren<GameObject>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
--- Assets/Scripts/Steal.cs
+++ Assets/Scripts/Steal.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Steal : MonoBehaviour
 {
+    GameObject childItem = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        childItem = this.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Item")
+        {
+            Debug.Log("Stealing...");
+            collision.transform.position = childItem.transform.position;
+            collision.transform.parent = this.gameObject.transform;
+            //Destroy(collision.gameObject);
+        }
     }
 }
