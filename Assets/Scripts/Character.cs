ecce099ad5971ba86c9b973fec54b3de215d7752
Marc Sevilleja <marc@sevilayha.com>
2020-05-18 00:19:29 -0700

    Implemented Knight stopping the thief if they stole

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] float waitKnightTime = 2f;
+    private bool knight = false;
+    private bool stolen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.name.Contains("Knight"))
+            knight = true;
+    }
+
+    public void CommitThievery(bool thief)
+    {
+        stolen = thief;
+        Debug.Log("Has stolen");
+    }
+
+    public bool GetThiefStatus()
+    {
+        return stolen;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Character" && collision.GetComponent<Character>().GetThiefStatus() == true && knight == true)
+        {
+            Debug.Log("Delivering Justice");
+            StartCoroutine(KnightJustice(collision.gameObject));
+        }
+    }
+
+    IEnumerator KnightJustice(GameObject thief)
+    {
+        thief.GetComponent<CharacterPathing>().AltMoveSpeed(0);
+        this.GetComponent<CharacterPathing>().AltMoveSpeed(0);
+        yield return new WaitForSeconds(waitKnightTime);
+
+        thief.GetComponent<CharacterPathing>().ResetMoveSpeed();
+        this.GetComponent<CharacterPathing>().ResetMoveSpeed();
+        Destroy(thief.transform.GetChild(0).gameObject);
+    }
+}
--- Assets/Scripts/Steal.cs
+++ Assets/Scripts/Steal.cs
@@ -9,13 +9,7 @@ public class Steal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        childItem = this.transform.GetChild(0).gameObject;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
+        //childItem = this.transform.GetChild(0).gameObject;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -23,9 +17,11 @@ public class Steal : MonoBehaviour
         if (collision.tag == "Item")
         {
             Debug.Log("Stealing...");
-            collision.transform.position = childItem.transform.position;
+            //collision.transform.position = childItem.transform.position;
+            collision.transform.position = Vector2.MoveTowards(collision.transform.position, this.gameObject.transform.position, 2 * Time.deltaTime);
             collision.transform.parent = this.gameObject.transform;
-            //Destroy(collision.gameObject);
+
+            GetComponent<Character>().CommitThievery(true);
         }
     }
 }
