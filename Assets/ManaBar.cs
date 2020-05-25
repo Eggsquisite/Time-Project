d90b66bda6e2fc672eba4592ee8efcff6614b230
Marc Sevilleja <marc@sevilayha.com>
2020-05-24 22:53:19 -0700

    Crappiest looking code but it makes the mana bar work

---
+++ Assets/ManaBar.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    [SerializeField] ObjectPlacement placer;
+    [SerializeField] Slider slider;
+    [SerializeField] float maxMana = 5f;
+    [SerializeField] float manaCd = 1f;
+
+    private float baseManaCd;
+    private int portalsInUse;
+    private bool usingMana, manaRestore, manaEmpty, manaThreshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseManaCd = manaCd;
+        SetMaxMana(maxMana);
+    }
+
+    private void SetMaxMana(float mana)
+    {
+        slider.maxValue = mana;
+        slider.value = mana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (usingMana)
+            UseMana();
+
+        if (slider.value < slider.maxValue && !usingMana)
+            ManaCooldown();
+
+        if (!usingMana)
+            ManaRestore();
+
+        if (manaEmpty)
+            OutOfMana();
+
+        if (slider.value > 0)
+            manaEmpty = false;
+
+        if (slider.value >= (0.2 * slider.maxValue))
+            manaThreshold = true;
+
+        if (manaThreshold)
+            ManaThreshold();
+    }
+
+    public void UseMana(bool status)
+    {
+        if (slider.value > 0)
+        {
+            usingMana = status;
+            portalsInUse++;
+            Debug.Log("Portals: " + portalsInUse);
+        }
+    }
+    private void UseMana()
+    {
+        manaRestore = false;
+        if (slider.value > 0)
+            slider.value -= Time.deltaTime * portalsInUse;
+        else if (slider.value <= 0)
+        {
+            slider.value = 0;
+            usingMana = false;
+            manaEmpty = true;
+        }
+    }
+
+    private void OutOfMana()
+    {
+        placer.OutOfMana();
+        manaEmpty = false;
+        portalsInUse = 0;
+    }
+
+    private void ManaThreshold()
+    {
+        placer.SetManaReady(true);
+        manaThreshold = false;
+    }
+
+
+    private void ManaCooldown()
+    {
+        if (manaCd > 0 && !manaRestore)
+        {
+            manaCd -= Time.deltaTime;
+            Debug.Log("Cooling Down mana");
+        }
+        else if (manaCd <= 0)
+        {
+            Debug.Log("Cooldown done");
+            manaCd = baseManaCd;
+            manaRestore = true;
+        }
+        else if (usingMana == true)
+            manaCd = baseManaCd;
+    }
+
+    private void ManaRestore()
+    {
+        if (slider.value < slider.maxValue && manaRestore)
+            slider.value += Time.deltaTime;
+        else if (slider.value >= slider.maxValue)
+        {
+            slider.value = slider.maxValue;
+            manaRestore = false;
+        }
+        else if (usingMana == true)
+            manaRestore = false;
+    }
+}
--- Assets/Scripts/New/ObjectPlacement.cs
+++ Assets/Scripts/New/ObjectPlacement.cs
@@ -6,11 +6,22 @@ public class ObjectPlacement : MonoBehaviour
 {
     [SerializeField] GameObject selectObj = null;
     [SerializeField] GameObject objPosition = null;
+    [SerializeField] GameObject manaBar = null;
+
+    private ManaBar mb;
     private GameObject tempObj;
+    private GameObject[] allObjs;
     private Color tmp;
-    private bool ready = false;
-    private bool placed = false;
+    private bool ready, placed, useMana, manaReady;
     private SpriteRenderer[] spriteObjs;
+    private int index;
+
+    private void Start()
+    {
+        manaReady = true;
+        mb = manaBar.GetComponent<ManaBar>();
+        allObjs = new GameObject[2];
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,11 +29,17 @@ public class ObjectPlacement : MonoBehaviour
         if (ready)
             tempObj.transform.position = objPosition.transform.position;
 
+        if (useMana)
+        {
+            mb.UseMana(true);
+            useMana = false;
+        }
+
         if (selectObj != null)
         {
             objPosition.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
 
-            if (Input.GetButtonDown("Fire2") && !ready && !placed)
+            if (Input.GetButtonDown("Fire2") && !ready && !placed && manaReady)
             {
                 tempObj = Instantiate(selectObj, objPosition.transform.position, Quaternion.identity);
                 tempObj.GetComponent<ParticleSystem>().Stop();
@@ -48,9 +65,12 @@ public class ObjectPlacement : MonoBehaviour
 
                 tempObj.GetComponent<CapsuleCollider2D>().enabled = true;
                 tempObj.GetComponent<ParticleSystem>().Play();
+                allObjs[index] = tempObj;
 
+                index++;
                 ready = false;
                 placed = true;
+                useMana = true;
             }
         }
     }
@@ -70,5 +90,23 @@ public class ObjectPlacement : MonoBehaviour
     public void SetSelected(GameObject obj)
     {
         selectObj = obj;
+    }
+
+    public void SetManaReady(bool status)
+    {
+        manaReady = status;
+    }
+
+    public void OutOfMana()
+    {
+        Debug.Log("OOM");
+        for (int i = 0; i < index; i++)
+        {
+            Destroy(allObjs[i].gameObject);
+        }
+
+        index = 0;
+        placed = false;
+        manaReady = false;
     }
 }
