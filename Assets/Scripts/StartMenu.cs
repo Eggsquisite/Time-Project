3e334ee8dcaeb67b8a4c411b85667cd51580e4e9
Marc Sevilleja <marc@sevilayha.com>
2020-05-25 23:20:33 -0700

    HOW TO PLAY DONE

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,21 +7,35 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] Button continueButt;
+    [SerializeField] Button nextButt;
     [SerializeField] GameObject controls;
     [SerializeField] GameObject controlList;
 
-    private int level = 0;
+    private List<Transform> theList;
+    private int level, index;
     private bool control;
 
     // Start is called before the first frame update
     void Start()
     {
+        theList = GetControlList();
         controls.SetActive(control);
         level = PlayerPrefs.GetInt("LevelProgress");
         if (level <= 1)
             continueButt.interactable = false;
         else if (level > 1)
             continueButt.interactable = true;
+    }
+
+    private List<Transform> GetControlList()
+    {
+        var tmp = new List<Transform>();
+
+        foreach (Transform child in controlList.transform)
+        {
+            tmp.Add(child);
+        }
+        return tmp;
     }
 
     public void NewGame()
@@ -43,7 +57,32 @@ public class StartMenu : MonoBehaviour
 
     public void Controls()
     {
-        control = !control;
-        controls.SetActive(control);
+        controls.SetActive(true);
+    }
+
+    public void Next()
+    {
+        if (index < theList.Count - 1)
+        {
+            theList[index].gameObject.SetActive(false);
+            index++;
+            theList[index].gameObject.SetActive(true);
+        }
+        
+        if (index >= theList.Count - 1)
+            nextButt.interactable = false;
+    }
+
+    public void Back()
+    {
+        if (index == 0)
+            controls.SetActive(false);
+        else
+        {
+            nextButt.interactable = true;
+            theList[index].gameObject.SetActive(false);
+            index--;
+            theList[index].gameObject.SetActive(true);
+        }
     }
 }
