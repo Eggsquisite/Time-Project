44807834f6afd74ca3ff4717bad2d1bad3ac7c46
Marc Sevilleja <marc@sevilayha.com>
2020-05-25 18:56:31 -0700

    Level Manager fixed by adding FindObject on buttons

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour
+{
+    LevelManager lm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lm = FindObjectOfType<LevelManager>();
+    }
+
+    public void LoadNextLevel()
+    {
+        lm.LoadNextLevel();
+    }
+
+    public void Retrylevel()
+    {
+        lm.RetryLevel();
+    }
+}
